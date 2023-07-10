01749c29ce22011e9cd8d2e0c3b50f0538fcccde
Alina Badillo <alinachristabel108@gmail.com>
2023-07-10 16:16:01 -0500

    Added divs for every hour up to 5m, also added current date

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+//Adding a current date in the header of the page
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.

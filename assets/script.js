235fb74c3bdfd1ef0586f730ffdcb7a6599e50a2
Alina Badillo <alinachristabel108@gmail.com>
2023-07-11 19:04:03 -0500

    Added fadeOut on a text which appears when we click Save button

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,22 @@
 $(function () {
-  //A button function with event listener
   $(".saveBtn").on('click', function() {
-    // var saveText = $('#saveText');
-    // var textLine = $('<p>');
-    // textLine.text('Appointment added to Local storage');
-    // saveText.append(textLine);
-
     //Added $(this) to save text from the particular line of text where we click save button
     var time = $(this).parent().attr("id").split('-')[1];
     var text = $(this).parent().find(".description").val();
 
     localStorage.setItem(time,text);
-});  
 
+    var saveText = $('#saveText');
+    var textLine = $('<p>');
+    textLine.text('Appointment added to Local storage✅');
+    saveText.append(textLine);
+
+    textLine.delay(1500).fadeOut(1000, function() {
+      textLine.remove();
+    });
+  });
+
+  //Shows time in 24-hour time
   var currentTime = dayjs().format('HH');
   //Used .each function to loop through each element
   $(".time-block").each(function () {
@@ -23,6 +27,7 @@ $(function () {
     var textArea = $(this).find('.description');
     textArea.val(textField);
 
+    //If statement; added '.description' to find this class in index.html
     if (timeBlock < currentTime) {
       $(this).find('.description').addClass("past");
     }
@@ -34,7 +39,6 @@ $(function () {
     }
   });
 
-  //Added code to display time on top of the page
   var todayDate = dayjs();
   $('#currentDay').text(todayDate.format('MMM D, YYYY'));
 });
